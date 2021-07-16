4433852e3e32af193b139d7688bf95bf41c26c36
Andrew Boslett <andrew_boslett@urmc.rochester.edu>
2021-07-16 10:29:45 -0400

    Established init conditions.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# shale-varying
-For work on the time-varying effects of the energy boom on communities.
+# Time-varying effects of the shale boom.
+
+In this repository, I explore the time-varying effects of the shale boom on communities in the U.S. My interest in this general topic is twofold: (1) I suspect that the economic effects of the shale boom - whether focused on labor, housing markets, etc., - have changed over time; and (2) I am the sort that requires an application to dive into the nuts-and-bolts of a new series of methods. 
+
+For the latter point, I am interested in learning about the methods discussed in the flood of research on difference-in-differences variants with time-varying treatment. I'm particularly interested in the decomposition method of Goodman-Bacon (2021). The shale boom did not occur in all communities in the U.S. at a single time period. Instead, the treatment was staggered from 2005-2012 across the states. 
+
+At this point, my interest is more on learning the method than the applied work. 
---
+++ scripts/0-initialization-file.R
@@ -0,0 +1,60 @@
+# Andrew Boslett
+# University of Rochester Medical Center/FLX AI
+# Email: andrew_boslett@urmc.rochester.edu
+# Permanent email: andrew.boslett@gmail.com
+
+# Set options
+
+options(scipen = 999)
+
+# Load packages
+# Note: I believe that all of these packages are used in this code (or were used in various iterations of it, some
+# of which may be deprecated).
+
+library(ggalt)
+library(tidyverse)
+library(foreign)
+library(readr)
+library(readxl)
+library(data.table)
+library(statar)
+library(sp)
+library(rgdal)
+library(stringr)
+library(fuzzyjoin)
+library(rlang)
+library(dplyr)
+library(magrittr)
+library(randomForest)
+library(gbm)
+library(boot)
+library(caret)
+library(xgboost)
+library(broom)
+library(ggrepel)
+library(glmnet)
+library(zoo)
+library(maps)
+library(tidycensus)
+library(vcd)
+
+# One directories of interest here:
+# (1) My personal directory, which connects my Box folder on my external hard-drive.
+
+# Set directory
+# Note: I set my working directory to the root of my research folder (which is synced in Box due to
+# organizational preferences). 
+
+setwd("C:/Users/aboslett/Box")
+
+# Set folder structure in your research directory.
+# Note: I create a project folder for data, scratch (e.g., intermediate files), and folders
+# for documents and figures created in support of the paper.
+
+dir.create('shale-varying', showWarnings = TRUE)
+
+for(fff in c('Data', 'Scratch', 'Documents', 'Figures', 'Tables')) {
+  
+  dir.create(paste0('shale-varying/', fff), showWarnings = TRUE)
+  
+}
